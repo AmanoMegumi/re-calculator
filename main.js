54dc9b7d9495ab1ccf1a954d7156b4af35c186d3
AmanoMegumi <34amano@gmail.com>
2022-04-19 12:58:46 +0900

    課題の再提出

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
+const operatorReg = /^(\+|-|\*|\/)$/;
+
 function get_calc(btn) {
-    if(btn.value == "=") {
+    const preValue = document.calculator.display.value.slice(-1);
+    let value = btn.value;
+
+    if(operatorReg.test(value) && operatorReg.test(preValue)) {
+        return;
+    }
+
+    if(btn.value === "=") {
         document.calculator.display.value = eval(document.calculator.display.value);
-    } else if (btn.value == "AC") {
+    } else if (btn.value === "AC") {
         document.calculator.display.value = "";
     } else {
         document.calculator.display.value += btn.value;
-        document.calculator.display.multi_btn.value = "*";
-        document.calculator.display.div_btn.value = "/";
     }
 }
